b27a6610dcff83b1c15d6e9e5444d9a16f317f85
IT25100984 - LAS Wijesinghe <it25100984@my.sliit.lk>
2026-04-28 09:01:50 +0530

    Update AppointmentDAO to implement interface

--- src/main/java/com/projectmass/dao/AppointmentDAO.java
+++ src/main/java/com/projectmass/dao/AppointmentDAO.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class AppointmentDAO {
+public class AppointmentDAO implements AppointmentDAOInterface {
 
     private final JdbcTemplate jdbcTemplate;
 
